fcb4864e65f40fe1246dfdb8996e32a5051742a2
Michael Viana <vmichael116@gmail.com>
2022-12-24 18:08:14 -0300

    Primeiro commit

---
+++ 1_variaveis/aula_1/script.js
@@ -0,0 +1,23 @@
+let nome = "Michael";
+
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = 'Guilherme';
+
+console.log(nome);
+
+laranjas = 4;
+
+console.log(laranjas);
+
+laranjas = "Laranja";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
